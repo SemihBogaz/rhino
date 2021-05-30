5dc21f51028efcb4ed908eff298d4ac348bc263c
SemihBogaz <20150807006@ogr.akdeniz.edu.tr>
2021-05-30 11:02:00 +0300

    Add sms verification Not working

--- app/src/main/java/com/infosec/rhino/MainActivity.java
+++ app/src/main/java/com/infosec/rhino/MainActivity.java
@@ -1,14 +1,103 @@
 package com.infosec.rhino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber,mVerificationCode;
+    private Button mSendCode;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    String mVerificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        userIsLoggedIn();
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mVerificationCode = findViewById(R.id.verificationCode);
+        mSendCode = findViewById(R.id.verify);
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull @org.jetbrains.annotations.NotNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull @org.jetbrains.annotations.NotNull FirebaseException e) {}
+
+            @Override
+            public void onCodeSent(@NonNull @NotNull String verificationId, @NonNull @NotNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(verificationId, forceResendingToken);
+
+                mVerificationId = verificationId;
+                mSendCode.setText("Verify Code");
+            }
+        };
+
+        mSendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mVerificationId != null){
+                    verifyPhoneNumber(mVerificationId,mVerificationCode.getText().toString());
+                }
+                else {
+                    startPhoneNumbeVerification();
+                }
+            }
+        });
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    userIsLoggedIn();
+                }
+            }
+        });
+    }
+
+    private void userIsLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            startActivity(new Intent(getApplicationContext(),MainPageActivity.class));
+            finish();
+        }
+    }
+
+    private void startPhoneNumbeVerification(){
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(mPhoneNumber.getText().toString(),200, TimeUnit.SECONDS,this,mCallbacks);
+    }
+
+    private void verifyPhoneNumber(String verificationId, String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId,code);
+        signInWithPhoneAuthCredential(credential);
+
     }
 }
