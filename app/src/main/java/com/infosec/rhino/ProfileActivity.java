54d6cdbfabcd61c608da047bcbbd101d4b9931cb
BekirNazligul <36674569+BekirNazligul@users.noreply.github.com>
2021-06-03 19:57:51 +0300

    Add users to DB.

--- app/src/main/java/com/infosec/rhino/ProfileActivity.java
+++ app/src/main/java/com/infosec/rhino/ProfileActivity.java
@@ -1,18 +1,27 @@
 package com.infosec.rhino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.infosec.rhino.databinding.ActivityProfileBinding;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
-
+    private DatabaseReference mDatabase;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -20,27 +29,50 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("test").setValue("Hello World");
         firebaseAuth = FirebaseAuth.getInstance();
         checkUserStatus();
 
-        binding.logOutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebaseAuth.signOut();
-                checkUserStatus();
-            }
+        super.onResume();
+        binding.saveBtn.setOnClickListener(v -> {
+            User user = new User(binding.userPhoneNumber.getText().toString(), binding.userName.getText().toString());
+            saveUser(user);
+        });
+
+        binding.logOutBtn.setOnClickListener(v -> {
+            firebaseAuth.signOut();
+            checkUserStatus();
         });
     }
+
 
     private void checkUserStatus() {
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
         if (firebaseUser != null){
             String phone = firebaseUser.getPhoneNumber();
+            //User user = getUser(phone);
             binding.userPhoneNumber.setText(phone);
+           //binding.userName.setText(user.getName());
         }
         else{
             finish();
         }
+    }
+
+    private void saveUser(User user) {
+        mDatabase.child("users").child(user.getPhoneNumber()).setValue(user.getName());
+    }
+
+    private User getUser(String phone) {
+        User result = new User(phone);
+        Task<DataSnapshot> snapshot = mDatabase.child("users").child(phone).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                String name = String.valueOf(Objects.requireNonNull(task.getResult()).getValue());
+                result.setName(name);
+            }
+        });
+        return  result;
     }
 }
---
+++ app/src/main/java/com/infosec/rhino/User.java
@@ -0,0 +1,35 @@
+package com.infosec.rhino;
+
+public class User {
+
+    private String phoneNumber, name;
+
+    public User() {
+
+    }
+
+    public User(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User(String phoneNumber, String name) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
